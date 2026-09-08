42ec8d387b53b1985bc9f9845e3a7994a138741a
kermokao <kermo.kao@voco.ee>
2026-09-08 11:35:57 +0000

    Mingi asjad

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 
 const App = () => {
+    const expenses = [
+        {
+            date: new Date(2024, 10, 12),
+            title: "New book",
+            price: 30.99
+        },
+        {
+            date: new Date(2024, 10, 12),
+            title: "New jeans",
+            price: 99.99
+        }
+    ];
+
     return (
         <div className="App">
-        <ExpenseItem/>
-
+            <ExpenseItem data={expenses[0]} />
+            <ExpenseItem data={expenses[1]} />
         </div>
     );
-}
+};
 
 export default App;
